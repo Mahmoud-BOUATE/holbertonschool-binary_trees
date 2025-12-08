d7ff7b94878103b9667eb0e4f31bb4a079fd6467
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-08 21:23:09 +0100

    test

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+binary_tree_t *tr;
 if (!parent)
 return (NULL);
-binary_tree_t *tr = malloc(sizeof(binary_tree_t));
+tr = malloc(sizeof(binary_tree_t));
 if (tr == NULL)
 return (NULL);
 
