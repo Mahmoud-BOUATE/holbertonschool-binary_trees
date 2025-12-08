1f20296a9a89d8ce262de99be1eb764ba1f244a5
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-08 22:00:35 +0100

    test

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,11 +7,12 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-
+if (node == NULL)
+return (NULL);
 if (node->left != NULL && node->right != NULL)
-return(0);
+return (0);
 if (node->right == NULL && node->left == NULL)
-return(1);
+return (1);
 
-return(0);
+return (0);
 }
