270e251299753afc4b970c7334007bc69d2e2f57
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-09 21:36:49 +0100

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,44 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- * Return: Height of the tree, 0 if tree is NULL
+ * binary_tree_height - Mesure la hauteur d'un arbre binaire
+ * @tree: Pointeur vers la racine de l'arbre
+ * Return: Hauteur de l'arbre, 0 si tree est NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t countL = 0, countR = 0;
+size_t left_height = 0, right_height = 0;
 
-if (!tree)
+if (tree == NULL)
 return 0;
 
-if (tree->left)
-countL = 1 + binary_tree_height(tree->left);
-if (tree->right)
-countR = 1 + binary_tree_height(tree->right);
+if (tree->left != NULL)
+left_height = binary_tree_height(tree->left);
 
-return (countL > countR ? countL : countR);
+if (tree->right != NULL)
+right_height = binary_tree_height(tree->right);
+
+if (left_height > right_height)
+return left_height + 1;
+else
+return right_height + 1;
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node
- * Return: Balance factor, 0 if tree is NULL
+ * binary_tree_balance - Mesure le facteur d'équilibre d'un arbre binaire
+ * @tree: Pointeur vers la racine de l'arbre
+ * Return: Facteur d'équilibre, 0 si tree est NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-size_t countL = 0, countR = 0;
-int def;
+size_t left_height = 0, right_height = 0;
 
-if (!tree)
+if (tree == NULL)
 return 0;
 
-if (tree->left)
-countL = binary_tree_height(tree->left);
-if (tree->right)
-countR = binary_tree_height(tree->right);
+if (tree->left != NULL)
+left_height = binary_tree_height(tree->left);
 
-def = (int)(countL - countR);
+if (tree->right != NULL)
+right_height = binary_tree_height(tree->right);
 
-return def;
+return (int)(left_height - right_height);
 }
